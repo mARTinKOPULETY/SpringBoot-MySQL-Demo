cbb5521c91ae59d6a79735df715d45f7f651dff3
mARTin <kopuletym@seznam.cz>
2022-04-06 11:52:06 +0200

    initialiazing  of Demo MySql project

---
+++ src/main/java/com/mARTin/SpringBootMySqlDemo/SpringBootMySqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mARTin.SpringBootMySqlDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMySqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMySqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mARTin/SpringBootMySqlDemo/SpringBootMySqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mARTin.SpringBootMySqlDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMySqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
